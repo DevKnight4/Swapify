096426ef1af9660c51610799fa92fcf216084b59
SurabhiSahu20 <167804552+SurabhiSahu20@users.noreply.github.com>
2025-07-12 12:07:51 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './client/pages/**/*.{ts,tsx}',
+    './client/components/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
